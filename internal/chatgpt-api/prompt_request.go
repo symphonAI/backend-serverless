883ae6ccbd16c0832017b3204df936b64e39a6a3
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-06-11 01:27:18 +1000

    Remove uneccesary logging

--- internal/chatgpt-api/prompt_request.go
+++ internal/chatgpt-api/prompt_request.go
@@ -39,31 +39,22 @@ func (c *Client) PromptChatGPT(userFields UserFields) (string, error) {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+c.apiKey)
 
-	fmt.Println("Req:", req)
-
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Resp:", resp)
-
 	responseBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-
-	fmt.Println("Resp:", string(responseBody))
 
 	response := ChatGPTResponse{}
 	err = json.Unmarshal(responseBody, &response)
 	if err != nil {
 		return "", err
 	}
-
-
-	fmt.Println("Resp:", response.Choices[0].Text)
 
 	return response.Choices[0].Text, nil
 }

ed1f3504041457f755d5c79e311a13750aaa0427
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-06-10 18:27:25 +1000

    adding error handling to signup-handler

--- go.mod
+++ go.mod
@@ -3,44 +3,38 @@ module backend-serverless
 go 1.18
 
 require (
-	github.com/aws/aws-cdk-go/awscdk/v2 v2.82.0
-	github.com/aws/constructs-go/constructs/v10 v10.2.26
+	github.com/aws/aws-sdk-go-v2 v1.18.0
+	github.com/aws/aws-sdk-go-v2/config v1.18.25
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.25
+	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.22.10
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.19.7
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.36.4
 	github.com/aws/jsii-runtime-go v1.82.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.18.0 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.18.25 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.25 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.33 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.27 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
-	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.22.10 // indirect
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.19.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.14.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.27 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.27 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssm v1.36.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.19.0 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
 
 require (
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
-	github.com/aws/aws-cdk-go/awscdk v1.203.0-devpreview // indirect
+	github.com/aws/aws-cdk-go/awscdk v1.203.0-devpreview
 	github.com/aws/aws-lambda-go v1.41.0
-	github.com/aws/constructs-go/constructs/v3 v3.4.232 // indirect
-	github.com/cdklabs/awscdk-asset-awscli-go/awscliv1/v2 v2.2.177 // indirect
-	github.com/cdklabs/awscdk-asset-kubectl-go/kubectlv20/v2 v2.1.1 // indirect
-	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv5/v2 v2.0.148 // indirect
+	github.com/aws/constructs-go/constructs/v3 v3.4.232
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
--- lambdas/signup-handler/main.go
+++ lambdas/signup-handler/main.go
@@ -1,9 +1,12 @@
 // main.go
 package main
 
+// hi sunny :^)
+
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -16,11 +19,12 @@ func handlePrompt(ctx context.Context, request events.APIGatewayProxyRequest) (e
 	requestBody := SignupRequestBody{}
 	err := json.Unmarshal([]byte(request.Body), &requestBody)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to unmarshal request body: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: 400,
-			Body:       "JSON key: 'code' missing from response",
+			Body:       errorString,
 		}
-		return response, nil 
+		return response, errors.New(errorString)
 	}
 
 	code := requestBody.Code
@@ -29,11 +33,12 @@ func handlePrompt(ctx context.Context, request events.APIGatewayProxyRequest) (e
 
 	refresh_token, access_token, err := exchangeCodeForAuthTokens(code)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to exchange auth code with refresh token: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       "Unable to exchange auth code with refresh token",
+			Body:      errorString,
 		}
-		return response, nil // TODO should return error instead?
+		return response, errors.New(errorString) // TODO should return error instead?
 	}
 	fmt.Println("Successfully generated refresh token and access token.")
 
@@ -43,40 +48,44 @@ func handlePrompt(ctx context.Context, request events.APIGatewayProxyRequest) (e
 	// Get User Email
 	id, email, err := getUserIdentifiers(access_token)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to get user identifiers: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       "Unable to get Spotify user identifiers",
+			Body:       errorString,
 		}
-		return response, nil 
+		return response, errors.New(errorString) 
 
 	}
 	// Save user in Cognito User Pool, retain User ID
 	err = saveUserToCognito(id, email)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to save user in Cognito user pool: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       "Unable to save user in Cognito user pool",
+			Body:       errorString,
 		}
-		return response, nil 
+		return response, errors.New(errorString)
 	}
 	
 	// Save User ID, Refresh token against this user in DB
 	saveUserAndRefreshTokenToDb(id, email, refresh_token)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to save user and refresh token to DB: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       "Unable to save user to DB",
+			Body:       errorString,
 		}
-		return response, nil 
+		return response, errors.New(errorString) 
 	}
 
 	jwToken, err := GenerateJWT("ap-southeast-2", email)
 	if err != nil {
+		errorString := fmt.Sprintf("unable to generate JWT: %s", err.Error())
 		response := events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError, 
-			Body: "Error creating JWT Token",
+			Body: errorString,
 		}
-		return response, nil
+		return response, errors.New(errorString)
 	}
 
 	fmt.Println("Successfully generated JWT. Returning JWT in response...")

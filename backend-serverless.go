3bafa5d038ea61efa8a35835978c9cfaca64f761
Sadruddin Junejo <sjunejo1@gmail.com>
2023-06-11 21:27:22 +1000

    Set allowCredentials to true

--- backend-serverless.go
+++ backend-serverless.go
@@ -49,12 +49,14 @@ func NewBackendServerlessStack(scope constructs.Construct, id string, props *Bac
 
 	// Create a new api HTTP api on gateway v2.
 	max_age_in_minutes := 10.00
+	allowCredentials := true
 	api := awsapigatewayv2.NewHttpApi(stack, jsii.String("symphonai-api"), &awsapigatewayv2.HttpApiProps{
 		CorsPreflight: &awsapigatewayv2.CorsPreflightOptions{
 			AllowOrigins: &[]*string{jsii.String("https://symphon.ai"), jsii.String("http://localhost:3000")}, // Provide a list of allowed origins
 			AllowMethods: &[]awsapigatewayv2.CorsHttpMethod{
 				awsapigatewayv2.CorsHttpMethod_ANY,
 			},
+			AllowCredentials: &allowCredentials,
 			AllowHeaders:  &[]*string{jsii.String("*")},
 			ExposeHeaders: &[]*string{jsii.String("*")},
 			MaxAge:        awscdk.Duration_Minutes(&max_age_in_minutes),
